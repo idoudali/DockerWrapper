cde4284e617299fa129c836f630ae61020cbb01c
Ioannis Doudalis <doudalis@gmail.com>
2022-02-18 19:35:49 -0800

    Format docker_wrapper.py with black and isort

--- src/docker_helpers/docker_wrapper.py
+++ src/docker_helpers/docker_wrapper.py
@@ -1,38 +1,41 @@
 #!/usr/bin/env python3
 import argparse
-import argcomplete
+import getpass
 import importlib
 import importlib.util
-import checksumdir
-import docker
 import logging
 import os
-import getpass
 import subprocess
 import sys
 
-_LOG_LEVEL_STRINGS = ['CRITICAL', 'ERROR', 'WARNING', 'INFO', 'DEBUG']
+import argcomplete
+import checksumdir
+import docker
+
+_LOG_LEVEL_STRINGS = ["CRITICAL", "ERROR", "WARNING", "INFO", "DEBUG"]
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(SCRIPT_DIR)
 
 DOCKER_FOLDERS = [
     os.path.realpath(os.path.join(root, dir))
-    for root, dirs, files in os.walk(os.getcwd(), topdown=True) for dir in dirs
-    if 'Docker' == dir
+    for root, dirs, files in os.walk(os.getcwd(), topdown=True)
+    for dir in dirs
+    if "Docker" == dir
 ]
 
 FOLDERS = [
-    os.path.realpath(os.path.join(i, '..')) for i in DOCKER_FOLDERS
-    if os.path.exists(os.path.join(i, '../docker_wrapper_extensions.py'))
+    os.path.realpath(os.path.join(i, ".."))
+    for i in DOCKER_FOLDERS
+    if os.path.exists(os.path.join(i, "../docker_wrapper_extensions.py"))
 ]
 
 WRAPPER_EXTENSIONS = {}
 
 for f in FOLDERS:
     spec = importlib.util.spec_from_file_location(
-        "docker_wrapper_extensions",
-        os.path.join(f, 'docker_wrapper_extensions.py'))
+        "docker_wrapper_extensions", os.path.join(f, "docker_wrapper_extensions.py")
+    )
     mod = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(mod)
     name = os.path.split(f)[1]
@@ -44,15 +47,12 @@ def image_hash(docker_file_path):
     contents of the Docker folder of the project
 
     Return  the 10 first digits"""
-    return checksumdir.dirhash(docker_file_path, 'sha1')[:10]
+    return checksumdir.dirhash(docker_file_path, "sha1")[:10]
 
 
 def build_image(docker_client, image_name, tag, docker_file_path):
     """Build the image"""
-    cmd = [
-        'docker', 'build', docker_file_path, '-t',
-        "{}:{}".format(image_name, tag)
-    ]
+    cmd = ["docker", "build", docker_file_path, "-t", "{}:{}".format(image_name, tag)]
     print(cmd)
     subprocess.check_call(cmd, stdout=sys.stdout, stderr=sys.stderr)
 
@@ -69,7 +69,7 @@ def image_exists(docker_client, image_name, tag):
 def run(args):
     docker_client = docker.from_env()
     project_path, extension = WRAPPER_EXTENSIONS[args.project]
-    docker_folder = os.path.join(project_path, 'Docker')
+    docker_folder = os.path.join(project_path, "Docker")
     image_name = args.project.lower()
     hash = image_hash(docker_folder)
     logging.debug("Dir Hash: {}".format(hash))
@@ -81,49 +81,55 @@ def run(args):
     gid = os.getgid()
     username = getpass.getuser()
     logging.debug("uid:{}, gid:{}, username:{}".format(uid, gid, username))
-    home_dir = os.path.expanduser('~')
-    cmd = ['nvidia-docker'] if args.enable_nvidia else ['docker']
-    cmd += ['run', '--rm', '--hostname=Docker']
+    home_dir = os.path.expanduser("~")
+    cmd = ["nvidia-docker"] if args.enable_nvidia else ["docker"]
+    cmd += ["run", "--rm", "--hostname=Docker"]
     if args.privileged:
-        cmd += ['--privileged']
+        cmd += ["--privileged"]
     if args.enable_gui:
         cmd += [
-            '-e',
-            'DISPLAY=$DISPLAY',
-            '-v',
-            '/tmp/.X11-unix:/tmp/.X11-unix',
+            "-e",
+            "DISPLAY=$DISPLAY",
+            "-v",
+            "/tmp/.X11-unix:/tmp/.X11-unix",
         ]
     if args.network:
         cmd += [f"--network={args.network}"]
     cmd += [
-        '-e', 'UID={}'.format(uid), '-e', 'GID={}'.format(gid), '-e',
-        'USERNAME={}'.format(username), '-e', f'SRC_DIR={project_path}'
+        "-e",
+        "UID={}".format(uid),
+        "-e",
+        "GID={}".format(gid),
+        "-e",
+        "USERNAME={}".format(username),
+        "-e",
+        f"SRC_DIR={project_path}",
     ]
     if args.prompt:
-        cmd += ['-t', '-i', '-e', 'PROMPT=1']
+        cmd += ["-t", "-i", "-e", "PROMPT=1"]
     if args.port:
         cmd += ["-p", "{}:{}".format(args.port, args.port)]
-    if hasattr(extension, 'add_docker_args'):
+    if hasattr(extension, "add_docker_args"):
         extension.add_docker_args(args, cmd)
-    cmd += [
-        '-v', home_dir + ":" + home_dir, '-v',
-        f'{project_path}:{project_path}', image
-    ]
-    if hasattr(extension, 'add_entrypoint_args'):
+    cmd += ["-v", home_dir + ":" + home_dir, "-v", f"{project_path}:{project_path}", image]
+    if hasattr(extension, "add_entrypoint_args"):
         extension.add_entrypoint_args(args, cmd)
     logging.info("Running cmd: {}".format(" ".join(cmd)))
-    subprocess.run(" ".join(cmd),
-                   stdin=sys.stdin,
-                   stdout=sys.stdout,
-                   stderr=sys.stderr,
-                   check=True,
-                   shell=True)
+    subprocess.run(
+        " ".join(cmd),
+        stdin=sys.stdin,
+        stdout=sys.stdout,
+        stderr=sys.stderr,
+        check=True,
+        shell=True,
+    )
 
 
 def _log_level_string_to_int(log_level_string):
     if not log_level_string in _LOG_LEVEL_STRINGS:
-        message = 'invalid choice: {0} (choose from {1})'.format(
-            log_level_string, _LOG_LEVEL_STRINGS)
+        message = "invalid choice: {0} (choose from {1})".format(
+            log_level_string, _LOG_LEVEL_STRINGS
+        )
         raise argparse.ArgumentTypeError(message)
     log_level_int = getattr(logging, log_level_string, logging.INFO)
     # check the logging log_level_choices have not changed from our expected values
@@ -134,39 +140,34 @@ def _log_level_string_to_int(log_level_string):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        '--log-level',
-        default='INFO',
-        dest='log_level',
+        "--log-level",
+        default="INFO",
+        dest="log_level",
         type=_log_level_string_to_int,
-        nargs='?',
-        help='Set the logging output level. {0}'.format(_LOG_LEVEL_STRINGS))
+        nargs="?",
+        help="Set the logging output level. {0}".format(_LOG_LEVEL_STRINGS),
+    )
     parser.add_argument(
-        '--prompt',
-        action='store_true',
-        help="Get a prompt inside the container for the project")
-    parser.add_argument('--enable-nvidia',
-                        action='store_true',
-                        default=False,
-                        help='Disable the use of nvidia-docker')
-    parser.add_argument('--network',
-                        default=None,
-                        help="Pass through option to the "
-                        "docker run network option")
-    parser.add_argument('--privileged',
-                        action='store_true',
-                        help='Enable privileged mode')
-    parser.add_argument('--enable-gui',
-                        action='store_true',
-                        help='Enable start gui apps')
-    parser.add_argument('--port',
-                        default="",
-                        help='Port to forward from docker')
+        "--prompt", action="store_true", help="Get a prompt inside the container for the project"
+    )
+    parser.add_argument(
+        "--enable-nvidia",
+        action="store_true",
+        default=False,
+        help="Disable the use of nvidia-docker",
+    )
+    parser.add_argument(
+        "--network", default=None, help="Pass through option to the " "docker run network option"
+    )
+    parser.add_argument("--privileged", action="store_true", help="Enable privileged mode")
+    parser.add_argument("--enable-gui", action="store_true", help="Enable start gui apps")
+    parser.add_argument("--port", default="", help="Port to forward from docker")
     subparsers = parser.add_subparsers(dest="project")
     subparsers.required = True
 
     for project, (_, mod) in WRAPPER_EXTENSIONS.items():
         subparser = subparsers.add_parser(project)
-        if hasattr(mod, 'register_subparser'):
+        if hasattr(mod, "register_subparser"):
             mod.register_subparser(subparser)
 
     argcomplete.autocomplete(parser)
