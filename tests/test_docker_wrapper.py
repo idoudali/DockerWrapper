48a323d5419b1cd7ba62738d934889772091ee39
Ioannis Doudalis <doudalis@gmail.com>
2022-02-19 17:16:16 -0800

    Add example tests
    This commit adds module pytest-mock to enable mocking classes with
    pytest and creates 2 really simple tests using the sample images
    we have.

--- tests/test_docker_wrapper.py
+++ tests/test_docker_wrapper.py
@@ -1,3 +1,33 @@
-# import pytest
+import os
+from pathlib import Path
+from typing import Callable, Dict
 
-# import docker_wrapper
+import pytest
+
+import docker_wrapper
+from docker_wrapper import docker_helpers
+
+
+@pytest.fixture
+def image_registry() -> Dict[str, Callable[[], docker_helpers.DockerImage]]:
+    # point to the sample_images directory
+    test_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "../sample-images")
+    ext = docker_wrapper.cli.find_extensions(Path(test_path))
+    return ext
+
+
+def test_image_hash(image_registry) -> None:  # type: ignore
+    assert "ubuntu-base" in image_registry
+    image = image_registry["ubuntu-base"]()
+    assert image.image_hash == "84ad86be99"
+    assert image.tagged_name == "ubuntu-base:84ad86be99"
+
+
+def test_image_prompt(image_registry, mocker) -> None:  # type: ignore
+    assert "ubuntu-base" in image_registry
+    image = image_registry["ubuntu-base"]()
+    mocker.patch.object(image, "_exec_cmd", autospec=True)
+    image.run("")
+    run_args = image._exec_cmd.call_args.args[0]
+    # print(run_args)
+    assert image.tagged_name in run_args
